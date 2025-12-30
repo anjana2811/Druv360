15345b3d4e1acd2b70c4e1d6c6fda9df14a0753a
anjana2811 <119124319+anjana2811@users.noreply.github.com>
2025-12-30 21:14:49 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,36 @@
+#x=5
+#if(x>10):
+ #   print("x is greater than 10")
+#else:
+ #   print("x is less than 10")
+
+
+#n=int(input("enter a number:"))
+#if(n%2==0):
+#    print("Number is even")
+#else:
+#    print("Number is odd")
+
+
+##n=int(input("enter a number"))
+##f=0
+##for i in range(2,n):
+##    if(n%i==0):
+##        f=1
+##        break
+##
+##if(f==1):
+##    print("it is not prime")
+##else:
+##    print("it is prime")
+##    
+##for i in range(1,11):
+##    print(i)
+
+
+for i in range(1,6):
+    for j in range(65,65+i):
+        print(chr(j),end='  ')
+    print()
+
+    
